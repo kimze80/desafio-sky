7b70cdea91749016805d4676b0cfa2bd87d92c04
kimze <kimze80@gmail.com>
2020-05-26 10:24:30 -0300

    js refactored and shopping icon added

--- src/scripts/movies.js
+++ src/scripts/movies.js
@@ -1,122 +1,49 @@
-fetch('https://sky-frontend.herokuapp.com/movies')
-  .then(res => res.json())
-  .then(json => {
-    json[2].movies.forEach(item => {
-      const horror = item.categories.indexOf("Terror");
-      const thriller = item.categories.indexOf("Suspense");
-      const list = `<div class="swiper-slide"><img src="${item.images[0].url}"></div>`
-      if (horror != -1 || thriller != -1) {
-        var swiper2 = new Swiper('.swiper2', {
-          slidesPerView: 'auto',
-          slidesPerGroup: 3,
-          spaceBetween: 10,
-          resistance: false,
-          navigation: {
-            nextEl: '.next',
-            prevEl: '.prev',
-          },
-          breakpoints: {
-            767: {
-              slidesPerGroup: 5,
-            },
-          }
-        });
-        swiper2.prependSlide(list);
-      }
-    });
-  })
-  .catch(() => {
-    $('#horror').html('Não há nenhum filme nessa categoria');
+
+function swiper(params, item) {
+  const list = `<div class="swiper-slide"><img src="${item.images[0].url}"><i class="fas fa-shopping-cart"></i></div>`
+  var params = new Swiper(`.${params}`, {
+    slidesPerView: 'auto',
+    slidesPerGroup: 3,
+    spaceBetween: 10,
+    resistance: false,
+    navigation: {
+      nextEl: '.next',
+      prevEl: '.prev',
+    },
+    breakpoints: {
+      767: {
+        slidesPerGroup: 5,
+      },
+    }
   });
+  params.prependSlide(list);
+}
 
-
-fetch('https://sky-frontend.herokuapp.com/movies')
-  .then(res => res.json())
-  .then(json => {
-    json[2].movies.forEach(item => {
-      const adventure = item.categories.indexOf("Ação e Aventura");
-      const list = `<div class="swiper-slide"><img src="${item.images[0].url}"></div>`
-      if (adventure != -1) {
-        var swiper3 = new Swiper('.swiper3', {
-          slidesPerView: 'auto',
-          slidesPerGroup: 3,
-          spaceBetween: 10,
-          resistance: false,
-          navigation: {
-            nextEl: '.next',
-            prevEl: '.prev',
-          },
-          breakpoints: {
-            767: {
-              slidesPerGroup: 5,
-            },
-          }
-        });
-        swiper3.prependSlide(list);
-      }
-    });
-  })
-  .catch(() => {
-    $('#adventure').html('Não há nenhum filme nessa categoria');
+$.get('https://sky-frontend.herokuapp.com/movies', data => {
+  data[2].movies.forEach(item => {
+    const horror = item.categories.indexOf("Terror");
+    const thriller = item.categories.indexOf("Suspense");
+    (horror != -1 || thriller != -1) && swiper('swiper2', item)
   });
+})
 
-
-fetch('https://sky-frontend.herokuapp.com/movies')
-  .then(res => res.json())
-  .then(json => {
-    json[2].movies.forEach(item => {
-      const comedy = item.categories.indexOf("Comédia");
-      const list = `<div class="swiper-slide"><img src="${item.images[0].url}"></div>`
-      if (comedy != -1) {
-        var swiper4 = new Swiper('.swiper4', {
-          slidesPerView: 'auto',
-          slidesPerGroup: 3,
-          spaceBetween: 10,
-          resistance: false,
-          navigation: {
-            nextEl: '.next',
-            prevEl: '.prev',
-          },
-          breakpoints: {
-            767: {
-              slidesPerGroup: 5,
-            },
-          }
-        });
-        swiper4.prependSlide(list);
-      }
-    });
-  })
-  .catch(() => {
-    $('#comedy').html('Não há nenhum filme nessa categoria');
+$.get('https://sky-frontend.herokuapp.com/movies', data => {
+  data[2].movies.forEach(item => {
+    const adventure = item.categories.indexOf("Ação e Aventura");
+    adventure != -1 && swiper('swiper3', item)
   });
+})
 
-  fetch('https://sky-frontend.herokuapp.com/movies')
-  .then(res => res.json())
-  .then(json => {
-    json[2].movies.forEach(item => {
-      const drama = item.categories.indexOf("Drama");
-      const list = `<div class="swiper-slide"><img src="${item.images[0].url}"></div>`
-      if (drama != -1) {
-        var swiper5 = new Swiper('.swiper5', {
-          slidesPerView: 'auto',
-          slidesPerGroup: 3,
-          spaceBetween: 10,
-          resistance: false,
-          navigation: {
-            nextEl: '.next',
-            prevEl: '.prev',
-          },
-          breakpoints: {
-            767: {
-              slidesPerGroup: 5,
-            },
-          }
-        });
-        swiper5.prependSlide(list);
-      }
-    });
-  })
-  .catch(() => {
-    $('#drama').html('Não há nenhum filme nessa categoria');
+$.get('https://sky-frontend.herokuapp.com/movies', data => {
+  data[2].movies.forEach(item => {
+    const comedy = item.categories.indexOf("Comédia");
+    comedy != -1 && swiper('swiper4', item)
   });
+})
+
+$.get('https://sky-frontend.herokuapp.com/movies', data => {
+  data[2].movies.forEach(item => {
+    const drama = item.categories.indexOf("Drama");
+    drama != -1 && swiper('swiper5', item)
+  });
+})
